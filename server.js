7f75c4f06c877bd5c09fb5682828fbc8a05c13eb
visa-sc <komizarenko@gmail.com>
2026-04-20 18:28:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => {
-  console.log("Сервер запущен: http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
 });
